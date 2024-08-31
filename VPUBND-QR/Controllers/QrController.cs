677b824aba087d883d5eb48899f85999b3f83e7a
Xuan Tinh Chu <43417661+bachkhoasoft@users.noreply.github.com>
2024-09-01 00:36:58 +0700

    Initial commit

---
+++ VPUBND-QR/Controllers/QrController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+
+namespace CreationQRCode.Controllers
+{
+    public class QrController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VPUBND(string text)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCoder.QRCode qrCode = new QRCoder.QRCode(qrCodeData);
+            string file = Guid.NewGuid().ToString().Substring(0, 5);
+            Bitmap qrCodeImage = qrCode.GetGraphic(8);
+
+            using (var stream = new MemoryStream())
+            {
+                qrCodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                
+                string fileGuid = Guid.NewGuid().ToString().Substring(0, 4);
+                qrCodeImage.Save("wwwroot/file/vpubndtphcm-" + fileGuid + ".png");
+                ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(stream.ToArray());
+                return File(stream.ToArray(), "image/png");
+               
+            }
+
+            //using (Bitmap bitMap = qrCode.GetGraphic(20))
+            //{
+            //    using (MemoryStream ms = new MemoryStream())
+            //    {
+            //        bitMap.Save(ms, ImageFormat.Png);
+            //        byte[] byteImage = ms.ToArray();
+            //        ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+            //    }
+            //    return View();
+            //}
+
+            return View();
+
+            //return View(BitmapToBytes(qrCodeImage));
+        }
+
+        [HttpPost]
+        public IActionResult Index(string text1)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(text1, QRCodeGenerator.ECCLevel.Q);
+                QRCoder.QRCode qrCode = new QRCoder.QRCode(qrCodeData);
+
+
+                using (Bitmap bitMap = qrCode.GetGraphic(20))
+                {
+                    bitMap.Save(ms, ImageFormat.Png);
+                    ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+
+
+
+            return View();
+        }
+
+
+
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        public IActionResult GenerateFile(string qrText)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            string fileGuid = Guid.NewGuid().ToString().Substring(0, 4);
+            qrCodeData.SaveRawData("wwwroot/qrr/file-" + fileGuid + ".qrr", QRCodeData.Compression.Uncompressed);
+
+            QRCodeData qrCodeData1 = new QRCodeData("wwwroot/qrr/file-" + fileGuid + ".qrr", QRCodeData.Compression.Uncompressed);
+            QRCode qrCode = new QRCode(qrCodeData1);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return View(BitmapToBytes(qrCodeImage));
+        }
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                img.Save("wwwroot/file/file-.png");
+
+                return stream.ToArray();
+            }
+        }
+    }
+
+
+}
